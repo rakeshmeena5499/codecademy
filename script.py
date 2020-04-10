b750f22fe3546510f38ffd61532ed07d86ce3e75
RKM <rakeshmeena5499@gmail.com>
2020-04-10 20:57:58 +0530

    Added interest finder logic

--- script.py
+++ script.py
@@ -35,10 +35,6 @@ def add_attraction(destination, attraction):
 		return
 
 add_attraction('Los Angeles, USA', ['Venice Beach', ['beach']])
-
-#print(attractions)
-
-
 add_attraction("Paris, France", ["the Louvre", ["art", "museum"]])
 add_attraction("Paris, France", ["Arc de Triomphe", ["historical site", "monument"]])
 add_attraction("Shanghai, China", ["Yu Garden", ["garden", "historcical site"]])
@@ -49,3 +45,25 @@ add_attraction("São Paulo, Brazil", ["São Paulo Zoo", ["zoo"]])
 add_attraction("São Paulo, Brazil", ["Pátio do Colégio", ["historical site"]])
 add_attraction("Cairo, Egypt", ["Pyramids of Giza", ["monument", "historical site"]])
 add_attraction("Cairo, Egypt", ["Egyptian Museum", ["museum"]])
+
+#print(attractions)
+
+
+def find_attractions(destination, interests):
+	destination_index=get_destination_index(destination)
+	attractions_in_city=attractions[destination_index]
+	attractions_with_interests=[]
+	for item in attractions_in_city:
+		possible_attraction=item
+		attractions_tags=item[1]
+		for interest in interests:
+			if interest in attractions_tags:
+				attractions_with_interests.append(possible_attraction[0])
+
+	return attractions_with_interests
+
+
+la_arts=find_attractions('Los Angeles, USA', ['art'])
+
+print(la_arts)
+
