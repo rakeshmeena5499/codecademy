961dfb4fb958228a1882df0367864955f1be70a3
RKM <rakeshmeena5499@gmail.com>
2020-04-10 19:34:32 +0530

    Added test objects

---
+++ script.py
@@ -0,0 +1,4 @@
+destinations=['Paris, France', 'Shanghai, China', 'Los Angeles, USA', 'São Paulo, Brazil', 'Cairo, Egypt']
+
+test_traveler=['Erin Wilked', 'Shanghai, China', ['hitorical site', 'art']]
+
